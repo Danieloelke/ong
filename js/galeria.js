3bd3131ffb6dbccb2947e6c6f59da2ab43abe207
Danieloelke <danielcordeirooelke@gmail.com>
2023-08-04 16:51:17 -0300

    add

--- js/galeria.js
+++ js/galeria.js
@@ -1,12 +1,22 @@
 
 // Supondo que você tenha uma array de fotos com informações
 const photos = [
+    { src: '../img/galeria/1.jpg', text: 'Foto 2 - Descrição' },
     { src: '../img/galeria/2.jpg', text: 'Foto 2 - Descrição' },
     { src: '../img/galeria/3.jpg', text: 'Foto 2 - Descrição' },
     { src: '../img/galeria/4.jpg', text: 'Foto 2 - Descrição' },
     { src: '../img/galeria/5.jpg', text: 'Foto 2 - Descrição' },
     { src: '../img/galeria/6.jpg', text: 'Foto 2 - Descrição' },
     { src: '../img/galeria/7.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/8.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/9.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/10.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/11.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/12.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/13.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/14.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/15.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/16.jpg', text: 'Foto 2 - Descrição' },
     // Adicione mais objetos de foto aqui
 ];
 
@@ -27,3 +37,4 @@ photos.forEach(photo => {
 
     galleryContainer.appendChild(photoDiv);
 });
+
