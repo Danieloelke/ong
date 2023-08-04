3fdbbb8afc31c63b8e3de70c7e55879fbc9d6e3f
Daniel oelke <117871005+Danieloelke@users.noreply.github.com>
2023-08-04 10:12:27 -0300

    add

--- js/galeria.js
+++ js/galeria.js
@@ -1,13 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var gallery = document.querySelector(".gallery");
-    var flkty = new Flickity(gallery, {
-      // Opções do Flickity, você pode personalizar aqui
-      cellAlign: "left",
-      contain: true,
-      wrapAround: true,
-      autoPlay: 3000, // Altere para o intervalo de tempo desejado (em milissegundos)
-      prevNextButtons: true,
-      pageDots: false
-    });
-  });
-  
+
+// Supondo que você tenha uma array de fotos com informações
+const photos = [
+    { src: '', text: 'Foto 1 - Descrição' },
+    { src: '../img/galeria/2.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/3.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/4.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/5.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/6.jpg', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/7.jpg', text: 'Foto 2 - Descrição' },
+    // Adicione mais objetos de foto aqui
+];
+
+const galleryContainer = document.querySelector('.photo-grid');
+
+photos.forEach(photo => {
+    const photoDiv = document.createElement('div');
+    photoDiv.classList.add('photo');
+
+    const img = document.createElement('img');
+    img.src = photo.src;
+    photoDiv.appendChild(img);
+
+    const content = document.createElement('div');
+    content.classList.add('photo-content');
+    content.textContent = photo.text;
+    photoDiv.appendChild(content);
+
+    galleryContainer.appendChild(photoDiv);
+});
