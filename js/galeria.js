197ef057d2c38a575df967d98b7c27f1973e311f
Danieloelke <danielcordeirooelke@gmail.com>
2023-08-30 14:33:04 -0300

    add

--- js/galeria.js
+++ js/galeria.js
@@ -1,26 +1,23 @@
 
 // Supondo que você tenha uma array de fotos com informações
 const photos = [
-    { src: '../img/galeria/1.jpeg', class:'exibe1', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/2.jpeg', class:'exibe2', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/3.jpg', class:'exibe3', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/4.jpg', class:'exibe4', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/5.jpg', class:'exibe5', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/6.jpg', class:'exibe6', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/7.jpg', class:'exibe7', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/8.jpg', class:'exibe8', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/9.jpg', class:'exibe9', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/10.jpg', class:'exibe10', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/11.jpg', class:'exibe11', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/12.jpg', class:'exibe12', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/13.jpg', class:'exibe13', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/14.jpg', class:'exibe14', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/15.jpg', class:'exibe15', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/16.jpg', class:'exibe16', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/17.jpg', class:'exibe17', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/18.jpg', class:'exibe18', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/19.jpg', class:'exibe19', text: 'Foto 2 - Descrição' },
-    { src: '../img/galeria/20.jpg', class:'exibe20', text: 'Foto 2 - Descrição' },
+    { src: '../img/galeria/1.jpeg', class:'exibe1'},
+    { src: '../img/galeria/2.jpeg', class:'exibe2'},
+    { src: '../img/galeria/3.jpg', class:'exibe3'},
+    { src: '../img/galeria/4.jpg', class:'exibe4'},
+    { src: '../img/galeria/5.jpg', class:'exibe5'},
+    { src: '../img/galeria/6.jpg', class:'exibe6'},
+    { src: '../img/galeria/7.jpg', class:'exibe7'},
+    { src: '../img/galeria/8.jpg', class:'exibe8'},
+    { src: '../img/galeria/9.jpg', class:'exibe9'},
+    { src: '../img/galeria/10.jpg', class:'exibe10'},
+    { src: '../img/galeria/11.jpg', class:'exibe11'},
+    { src: '../img/galeria/12.jpg', class:'exibe12'},
+    { src: '../img/galeria/14.jpg', class:'exibe14'},
+    { src: '../img/galeria/15.jpg', class:'exibe15'},
+    { src: '../img/galeria/18.jpg', class:'exibe18'},
+    { src: '../img/galeria/19.jpg', class:'exibe19'},
+
     // Adicione mais objetos de foto aqui
 ];
 
